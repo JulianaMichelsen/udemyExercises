d8665cdf3612e822f3bd5ef7bead0512b96687c6
JulianaMichelsen <juju.michelsen@gmail.com>
2025-09-26 08:02:49 -0300

    Primeiros exercícios

--- src/main/java/exercises/exercise1.java
+++ src/main/java/exercises/exercise1.java
@@ -1,4 +1,25 @@
+/*
+Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+mensagem explicativa, conforme exemplos.
+ */
+
 package exercises;
 
+import java.util.Scanner;
+
 public class exercise1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
+
+        int soma = number1 + number2;
+
+        System.out.println("SOMA = " + soma);
+
+        sc.close();
+    }
 }
--- src/main/java/exercises/exercise2.java
+++ src/main/java/exercises/exercise2.java
@@ -1,4 +1,30 @@
+/*
+Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+casas decimais conforme exemplos.
+ Fórmula da área: area = π . raio2
+ Considere o valor de π = 3.14159
+ */
+
 package exercises;
 
+import java.util.Scanner;
+import java.util.Locale;
+import static java.lang.Math.pow;
+
 public class exercise2 {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+
+        double radius = sc.nextDouble();
+        double pi = 3.14159;
+        double circleArea = pi * pow(radius, 2);
+
+        System.out.printf("A=%.4f%n", circleArea);
+
+        sc.close();
+    }
 }
