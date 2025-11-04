aa0aa874c00491a08a201038dfc8b59d83950b0a
JulianaMichelsen <juju.michelsen@gmail.com>
2025-11-04 19:50:13 -0300

    Correção da resolução do Desafio de Lista

--- src/main/java/exerciseList/exercise1.java
+++ src/main/java/exerciseList/exercise1.java
@@ -16,57 +16,60 @@ class Employee {
     }
 }
 
-
 public class exercise1 {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("How many employees will be registred? ");
+        System.out.print("How many employees will be registered? ");
         int quantity = scanner.nextInt();
         scanner.nextLine();
 
         Employee[] employees = new Employee[quantity];
 
         for (int i = 0; i < quantity; i++) {
-            scanner.nextLine();
             System.out.println("Employee #" + (i + 1) + ":");
+
             System.out.print("Id: ");
             int id = scanner.nextInt();
             scanner.nextLine();
+
             System.out.print("Name: ");
             String name = scanner.nextLine();
+
             System.out.print("Salary: ");
             double salary = scanner.nextDouble();
             scanner.nextLine();
+
             employees[i] = new Employee(id, name, salary);
         }
 
-
         Arrays.sort(employees, (n1, n2) -> n1.id - n2.id);
 
-        scanner.nextLine();
         System.out.print("Enter the employee id that will have salary increase: ");
         int idIncrease = scanner.nextInt();
         System.out.print("Enter the percentage: ");
         double percentage = scanner.nextDouble();
 
+        boolean found = false;
         for (Employee emp : employees) {
             if (emp.id == idIncrease) {
                 emp.salary += emp.salary * percentage / 100.0;
+                found = true;
                 break;
             }
         }
 
+        if (!found) {
+            System.out.println("Employee id " + idIncrease + " does not exist!");
+        }
+
         System.out.println("\nList of employees:");
-        for (Employee  emp : employees) {
-            System.out.println(emp.id + ", " + emp.name + ", " + emp.salary);
+        for (Employee emp : employees) {
+            System.out.printf("%d, %s, %.2f%n", emp.id, emp.name, emp.salary);
         }
 
         scanner.close();
     }
-
-
-
 }
